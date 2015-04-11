dac8000577532da057c6f3722cf405cd36ddb8b3
wsupWorld <junyuw@berkeley.edu>
2015-04-11 12:50:58 -0700

    vote model/controller

---
+++ app/models/vote.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: votes
+#
+#  id          :integer          not null, primary key
+#  user_id     :integer
+#  question_id :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
+class Vote < ActiveRecord::Base
+  belongs_to: User 
+  belongs_to: Question
+end
