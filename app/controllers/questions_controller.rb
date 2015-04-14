c4c844c3477912903024af31ba0ff430ef9ae43a
wsupWorld <junyuw@berkeley.edu>
2015-04-13 19:49:47 -0700

    ordered by id

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,7 @@ class QuestionsController < ApplicationController
   
   def index
 #    @questions = Question.all
-    @questions = Question.paginate(:page => params[:page], :per_page => 3)
+    @questions = Question.paginate(:page => params[:page], :per_page => 3).order("id DESC")
   end
 
   def show
