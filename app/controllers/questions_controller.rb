0fecdc56d603e8bdeb7a4cf75f022ce1020f9863
wsupWorld <junyuw@berkeley.edu>
2015-04-13 18:59:47 -0700

    delted unused code and debugging on heroku

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,23 +1,5 @@
 class QuestionsController < ApplicationController
 
-  def init 
-    require 'rest_client'
-    @response = RestClient.get 'https://www.kimonolabs.com/api/2tepoq7s?apikey=i4ShqnRlMKnYLTe9oGyIOyef9ybz0Qnp'
-    @json = JSON.parse @response
-    @questions = @json["results"]["Questions"]
-
-    @questions.each do |q|
-      if q["Content"]["text"].length == 0
-        next
-      end
-      @question = Question.new
-      @question.content = q["Content"]["text"]
-
-      @question.source_url = q["Content"]["href"]
-      @question.vote_count = 0
-      @question.save!
-    end
-  end
   
   def index
 #    @questions = Question.all
